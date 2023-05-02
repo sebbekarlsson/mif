969333e186e0204a0f6e8b843c93fedc7aaeca58
sebastian karlsson <sebbekarlsson97@gmail.com>
2023-05-02 02:43:07 +0200

    fix typo

--- src/utils.c
+++ src/utils.c
@@ -190,7 +190,7 @@ uint32_t mif_float_bits_to_uint(float f) {
 float mif_random_float(float min, float max, float seed) {
   uint32_t s = (11U+mif_float_bits_to_uint(seed)) * 5358U;
   uint32_t x = ~s * 3U;
-  uint32_t y = 503 * (x << 3U) + s;
+  uint32_t y = 503U * (x << 3U) + s;
   s ^= (s << 17U); s ^= (s >> 13U); s ^= (s << 5U);
   x ^= (x << 4U); x ^= (x >> 2U); x ^= (x << 15U);
   y ^= (y << 11U); y ^= (y >> 9U); y ^= (y << 3U);
