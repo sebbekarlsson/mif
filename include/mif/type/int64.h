7ce38a1069369d3df7edc654ddb58c658afb6574
sebastian karlsson <sebbekarlsson97@gmail.com>
2023-12-15 23:16:25 +0100

    int64_t_buffer_includes

--- include/mif/type/int64.h
+++ include/mif/type/int64.h
@@ -2,5 +2,9 @@
 #define MIF_TYPE_INT64_H
 #include <mif/type/buffer.h>
 #include <stdint.h>
+#include <stdbool.h>
 MIF_DEFINE_BUFFER(int64_t);
+
+
+bool mif_int64_t_buffer_includes(int64_tBuffer buffer, int64_t value);
 #endif
--- src/type/int64.c
+++ src/type/int64.c
@@ -4,3 +4,13 @@
 #include <stdlib.h>
 
 MIF_IMPLEMENT_BUFFER(int64_t);
+
+bool mif_int64_t_buffer_includes(int64_tBuffer buffer, int64_t value) {
+  if (buffer.length <= 0 || buffer.items == 0) return false;
+
+  for (int64_t i = 0; i < buffer.length; i++) {
+    if (buffer.items[i] == value) return true;
+  }
+
+  return false;
+}
